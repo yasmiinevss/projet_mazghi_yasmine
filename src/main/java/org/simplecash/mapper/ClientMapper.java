520e846dbd56069853b67768c11693813f7643f3
yasmiinevss <yasmine.mazghi@epita.fr>
2025-11-28 13:43:38 +0100

    [FEAT] : Step 4 -> mapper

--- src/main/java/org/simplecash/mapper/ClientMapper.java
+++ src/main/java/org/simplecash/mapper/ClientMapper.java
@@ -1,4 +1,27 @@
 package org.simplecash.mapper;
 
-public class ClientMapper {
+import org.mapstruct.*;
+import org.simplecash.dto.ClientCreateDto;
+import org.simplecash.dto.ClientDto;
+import org.simplecash.dto.ClientUpdateDto;
+import org.simplecash.entity.Client;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ClientMapper {
+
+
+    @Mapping(source = "conseiller.nom", target = "nomConseiller")
+    ClientDto toDto(Client client);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "comptes", ignore = true)
+    @Mapping(target = "conseiller", ignore = true)
+    Client toEntity(ClientCreateDto dto);
+
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "comptes", ignore = true)
+    @Mapping(target = "conseiller", ignore = true)
+    void updateEntity(@MappingTarget Client entity, ClientUpdateDto dto);
 }
