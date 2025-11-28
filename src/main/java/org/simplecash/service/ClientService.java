f141138090af1dd0e1f792043174fe396465a54a
yasmiinevss <yasmine.mazghi@epita.fr>
2025-11-28 14:25:47 +0100

    [FEAT] : Service -> methodes de base (find, update, save)

--- src/main/java/org/simplecash/service/ClientService.java
+++ src/main/java/org/simplecash/service/ClientService.java
@@ -1,4 +1,18 @@
 package org.simplecash.service;
 
+import org.simplecash.dto.ClientCreateDto;
+import org.simplecash.dto.ClientDto;
+import org.simplecash.dto.ClientUpdateDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ClientService {
+    List<ClientDto> findAll();
+    ClientDto save(ClientCreateDto dto);
+    Optional<ClientDto> findById(Long id);
+    Optional<ClientDto> update(Long id, ClientUpdateDto dto);
+
+    //Fonction qui fait les virement
+    void virement(Long idCompteEmetteur, Long idCompteBeneficiaire, Double montant);
 }
--- src/main/java/org/simplecash/service/ClientServiceImpl.java
+++ src/main/java/org/simplecash/service/ClientServiceImpl.java
@@ -1,4 +1,74 @@
 package org.simplecash.service;
 
-public class ClientServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.simplecash.dto.ClientCreateDto;
+import org.simplecash.dto.ClientDto;
+import org.simplecash.dto.ClientUpdateDto;
+import org.simplecash.entity.Client;
+import org.simplecash.entity.Conseiller;
+import org.simplecash.mapper.ClientMapper;
+import org.simplecash.repository.ClientRepository;
+import org.simplecash.repository.CompteRepository;
+import org.simplecash.repository.ConseillerRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class ClientServiceImpl implements ClientService {
+    private final ClientRepository clientRepo;
+    private final CompteRepository compteRepo;
+    private final ConseillerRepository conseillerRepo;
+    private final ClientMapper mapper;
+
+    @Override
+    public List<ClientDto> findAll() {
+        return clientRepo.findAll().stream().map(mapper::toDto).toList(); // Recup la liste de tous les clients
+    }
+
+    @Override
+    public ClientDto save(ClientCreateDto dto) {
+        // D'abord trouver le conseiller sinon ton entite sera cree sans l'ID du conseiller en qst -> il n'y aura pas de lien
+        Conseiller conseiller = conseillerRepo.findById(dto.conseillerId()).orElseThrow(() -> new RuntimeException("Conseiller non trouve..."));
+
+        Client entity = mapper.toEntity(dto);
+        entity.setConseiller(conseiller);
+
+        Client saved = clientRepo.save(entity);
+        return mapper.toDto(saved);
+    }
+
+    @Override
+    public Optional<ClientDto> findById(Long id) {
+        return clientRepo.findById(id).map(mapper::toDto);    }
+
+    @Transactional // il permet de garder ouvert le contexte de persistance pendant toute la methode update
+    @Override
+    public Optional<ClientDto> update(Long id, ClientUpdateDto dto) {
+        return clientRepo.findById(id).map(
+                existingClient -> {
+
+                    // MAJ des champs existants
+                    mapper.updateEntity(existingClient, dto);
+
+                    // Si conseiller changed :
+                    if (dto.conseillerId() != null) {
+                        Conseiller c = conseillerRepo.findById(dto.conseillerId())
+                                .orElseThrow(() -> new RuntimeException("Conseiller non trouve " ));
+
+                        existingClient.setConseiller(c);
+                    }
+                    return mapper.toDto(existingClient);
+                }
+        );
+    }
+
+    @Override
+    public void virement(Long idCompteEmetteur, Long idCompteBeneficiaire, Double montant) {
+
+    }
 }
