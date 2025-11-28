8d0b2f3543e273910702f1c5a5587783b5c69304
yasmiinevss <yasmine.mazghi@epita.fr>
2025-11-28 10:57:46 +0100

    [FEAT] : Step 1 -> architecture

---
+++ src/main/java/org/simplecash/ProjetMazghiYasmineApplication.java
@@ -0,0 +1,13 @@
+package org.simplecash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMazghiYasmineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetMazghiYasmineApplication.class, args);
+    }
+
+}
